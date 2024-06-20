b29f8ef135663d95cfedf563235d7e97adc4c5bc
termusred <shohakbarabidov280@gmail.com>
2024-06-20 13:43:33 +0500

    dina mike tu to rial:

---
+++ app/(auth)/jobs/page.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Loader } from "react-feather";
+
+export default function SeekFor() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [def , setdef] = useState([{name:"gyat" , oylik:100000},{name:"ish" , oylik:9000},{name:"ish" , oylik:2000},{name:"ish" , oylik:3000}])
+    const [after , setAfter] = useState(def)
+    
+    function handleSearch(e) {
+        e.preventDefault()
+        def.map((el)=> {
+            if(el.name.includes(e.target[0].value)){
+                setAfter([el])
+            } else{
+                setAfter(def)
+            }
+        })
+        setIsLoading(true);
+        setTimeout(() => {
+            setIsLoading(false)
+        } , 1000)
+    }
+
+    return (
+        <div className="w-full h-screen bg-black flex flex-col items-center mt-24 p-8 gap-10">
+            <form onSubmit={(e) => handleSearch(e)} className="SearchCon w-1/2 flex gap-5">
+                <input className="w-full h-10 rounded-sm text-black" type="search" placeholder="Search..." required/>
+                <button className="bg-indigo-600 p-2 rounded-sm hover:bg-indigo-800 transition-all duration-75">
+                    Search
+                </button>
+            </form>
+            {isLoading && (
+                <div className="flex justify-center items-center mt-10">
+                    <Loader size={64} className="animate-spin" />
+                </div>
+            )}
+            <div className=" w-full flex gap-4 flex-wrap">
+                {!isLoading && after.map((el ,ind) =>{
+                    return(
+                        <div className="Container w-1/4 h-40 bg-gray-400 p-3" key={ind}>
+                            <h1>{el.name}</h1>
+                            <h1>{el.oylik}</h1>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    );
+}
--- app/(auth)/user/page.tsx
+++ app/(auth)/user/page.tsx
@@ -1,20 +1,34 @@
 /** @format */
 'use client';
-
-import Link from "next/link"
-import { ToastContainer, toast } from 'react-toastify';
+import { getItem } from '@/utils/storage';
+import Avatar from 'react-avatar';
 import 'react-toastify/dist/ReactToastify.css';
-import {setCookie} from '@/utils/cookies';
 
-export default function SignIn() {
+
+export default function User() {
   return (
     <div className=" mt-20">
-        <div className=" w-full h-96 bg-white">
-            <div className="w-38 h-38 bg-emerald-200"></div>
-        </div>
-        <div className=" w-full h-96 bg-white">
+        <div className=" w-full h-96 p-4 flex text-black justify-between">
+            <Avatar name="Sizning rasm" src='https://secure.gravatar.com/avatar/8c5d4c4b9ef6c68c4ff91c319d4c56be?d=404&s=512' size='248' className='ml-14'/>
+            <div className='flex flex-col w-1/2 text-white'>
+              <h1 className=' text-4xl'>Abidov Shohakbar</h1>
+              <h3 className=' text-xl'>Kasb : Front-end devoleper</h3>
+              <h3 className=' text-xl'>Daraja : Junior</h3>
+              <h3 className=' text-xl'>Tajriba : 12 oy</h3>
 
+            </div>
         </div>
+        <h2 className=' ml-12 text-xl'>Foydalanuvchi loyihalari</h2>
+        <div className=' flex h-96 flex-wrap m-7 gap-2'>
+          {getItem("Projects").map((el) => {
+            return(
+              <div className=' flex w-1/4 h-1/2 bg-black justify-items-center items-center p-8'>
+                <h1>Project nomi: </h1>
+                <h1>{el.name}</h1>
+              </div>
+            )
+          })}
+        </div>  
     </div>
   );
 }
